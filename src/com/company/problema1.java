37db130f3d3bd37b5d776c3669662f8874f9094f
sbravor64@elpuig.xeill.net <19972208>
2018-10-30 16:45:56 +0100

    problema1

--- src/com/company/problema1.java
+++ src/com/company/problema1.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class problema1 {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner teclado = new Scanner(System.in);
+        int uvas = 4354;
+
+        while(uvas != 0) {
+            uvas = teclado.nextInt();
+
+            int niveles = 0;
+            int totaluvas = 0;
+
+            while (uvas > totaluvas) {
+                niveles++;
+                totaluvas = niveles + totaluvas;
+            }
+            if(totaluvas != 0)
+                System.out.println(niveles);
+        }
     }
 }
